2454277f2a9d1f578f3a751858542af5707cb276
Vignesh <vigneshvicky8888vicky@gmail.com>
2026-06-05 01:58:47 +0530

    Add files via upload

---
+++ python_day_1_revision.py
@@ -0,0 +1,50 @@
+#show last number
+'''num=64532
+last_number=num%10
+print(last_number)'''
+#remove the last number 
+"""num=645328
+remove_last_number=num//10
+print(remove_last_number)"""
+#find the last two digit
+'''num=64532
+last_digit_find_it=num%100
+print(last_digit_find_it)'''
+#middle value square pananum
+'''num=6453278
+middle=(num//1000)%10
+square=middle**2
+print('square_of',middle,'is',square,)'''
+#BMI unga body height etha wight erukanu pakurathu
+'''weight=55
+hight=171
+bmi=weight/((hight/100)**2)
+print(bmi)'''
+#expand number
+'''num=9876
+thousend=(num//1000)*1000
+hundrad=(num//100)*100
+ten=((num//10)%10)*10
+one=num%10
+print(thousend,"+",hundrad,"+",ten,"+",one)'''
+#volume of cylinder
+''''radius=10
+height=22
+volume=3.14*radius*radius*height
+print("Volume",volume)'''
+#volume of cube
+'''length=8
+height=6
+breath=3
+volume=length*height*breath
+print("Volume=",volume)'''
+#convert centemeter to meter
+'''cm=267
+meter=cm/100
+print("Meter=",meter)'''
+#speed caclutor
+distance=200
+time=3
+speed=distance/time
+print("Speed",speed)
+
