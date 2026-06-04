114088c3cf4c7fc22e3425229b63d6c9230d0468
Vignesh <vigneshvicky8888vicky@gmail.com>
2026-06-05 01:56:11 +0530

    1.1
    revistion

---
+++ revistion.py
@@ -0,0 +1,116 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+2
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+6453
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+32
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+532
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+4532
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+64532
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+0
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+2
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+32
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py", line 15, in <module>
+    middle(num//100)%10
+NameError: name 'middle' is not defined
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py", line 15, in <module>
+    middle(num//100)%10
+NameError: name 'middle' is not defined
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py", line 15, in <module>
+    middle(num//100)%10
+NameError: name 'middle' is not defined
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+square_of 5 is 25
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+square_of 3 is 9
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+square_of 2 is 4
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+square_of 3 is 9
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+BMI=
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py", line 22, in <module>
+    print('BMI=',bmi)
+NameError: name 'bmi' is not defined. Did you mean: 'BMI'?
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+BMI= 18.80920625149619
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+BMI= 16.08187134502924
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+18.80920625149619
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py", line 29, in <module>
+    one(num%10)
+NameError: name 'one' is not defined. Did you mean: 'None'?
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py", line 29, in <module>
+    one(num%10)
+NameError: name 'one' is not defined. Did you mean: 'None'?
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+98000 + 98000 + 98700 + 60 + 5
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+9000 + 9800 + 70 + 6
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+volume=
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+Volume 6908.0
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+Volume 2049836.2145061572
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+Volume 6908.0
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+Volume= 144
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+Meter= 2.67
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python314/python_day_1_revision.py
+Speed 66.66666666666667
